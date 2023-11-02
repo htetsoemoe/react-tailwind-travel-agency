f2b98d9ae57148b505038408240b2c2b00a4a765
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-02 15:28:48 +0630

    project setup and running

---
+++ travel-agency/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold p-10 max-w-[1240px] mx-auto">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App

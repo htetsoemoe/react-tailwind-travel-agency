1667cddce1e70cf885ae616a8181c546ec24d588
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-11-07 10:34:25 +0630

    finished navbar component

--- travel-agency/src/App.jsx
+++ travel-agency/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopBar from './components/TopBar'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
       <TopBar />
+      <Navbar />
     </div>
   )
 }
--- travel-agency/src/components/TopBar.jsx
+++ travel-agency/src/components/TopBar.jsx
@@ -18,11 +18,12 @@ const TopBar = () => {
                         className='mr-2 text-[var(--primary-dark)]' />
                     <p className="text-sm text-gray-700">9AM - 5AM</p>
                 </div>
-                <div className="hidden md:flex items-center px-6">
+                <div className="hidden md:flex items-center px-6 pr-0">
                     <AiFillPhone
                         size={20}
                         className='mr-2 text-[var(--primary-dark)]' />
-                    <p className="text-sm text-gray-700">09 1111 23 447</p>
+                    <p className="text-sm text-gray-700 mr-5">09 1111 23 447</p>
+                    <button>Get a Free Quote</button>
                 </div>
             </div>
         </div>
